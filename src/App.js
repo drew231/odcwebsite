e839f6329aee5ad61f814d7e97da91baefd4284e
Andrew Livingstone, Jr <andrewlivingstonejr@gmail.com>
2022-12-09 15:59:05 +0000

    Verified

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+ 
+    <div className='hero-bg'>
+      <div className='container-fluid'>
+        <img url='' />
+        <a href='#'><button class="button" > Sign Up </button></a>
+        <a href='#'><button class="button" > Login </button></a>
+      </div>
+
+      <div className='container'>
+        <h2>Welcome to the</h2>
+        <h1>Orange Digital Center</h1>
+        <pre> &lt;coding.school &#47;&gt;  </pre>
+
+        <a className='sec2btn' href='#'><button class="button" > Login </button></a>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default function footer(){
+    return (
+        <div>lorem</div>
+    );
+}
