2a213156d35d1ca35b5779167d292be98d388fbd
Andrew Livingstone, Jr <104835802+drew231@users.noreply.github.com>
2022-12-12 17:37:01 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -8,17 +8,7 @@ function App() {
  
     <div className='hero-bg'>
       <div className='container-fluid'>
-        <img url='' />
-        <a href='#'><button class="button" > Sign Up </button></a>
-        <a href='#'><button class="button" > Login </button></a>
-      </div>
-
-      <div className='container'>
-        <h2>Welcome to the</h2>
-        <h1>Orange Digital Center</h1>
-        <pre> &lt;coding.school &#47;&gt;  </pre>
-
-        <a className='sec2btn' href='#'><button class="button" > Login </button></a>
+      
 
       </div>
     </div>
