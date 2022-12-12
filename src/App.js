8285a651885d063929843a4de76f4a999a4c98b1
Andrew Livingstone, Jr <andrewlivingstonejr@gmail.com>
2022-12-12 23:15:03 +0000

    Succeed

--- src/App.js
+++ src/App.js
@@ -9,21 +9,23 @@ function App() {
       <div className='container-fluid'>
 
         <div className='row'>
-          <div className='col-md-6'>
+          <div className='col-md-6 colon1'>
             <Slider />
           </div>
 
           <div className='col-md-6 colon2'>
             <div className='colon2_a'>
-              <ul>
-              <button className='btn0'>Sing In</button>
-              </ul>
               <h2>Welcome to the</h2>
               <h1>Orange Digital <br />Center</h1>
               <p> &lt;coding.school &#47; &gt; </p>
             </div>
+            <ul className='text-center mt-5'>
+              <button className='btn0'>Get Started</button> 
+              <button className='btn0'>Sign In</button>
+            </ul>
           </div>
         </div>
+
 
       </div>
 
